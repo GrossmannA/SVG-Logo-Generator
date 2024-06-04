4862864d77af69b47f97a0159887afa25a272843
grossmannA <agrossmann315@gmail.com>
2024-06-04 00:16:44 -0400

    updated to functioning application

---
+++ index.js
@@ -0,0 +1,62 @@
+const inquirer = require('inquirer');
+const fs = require('fs')
+const {Circle, Triangle, Square} = require("./lib/shapes");
+
+// inquirer questions that take in data to feed to rendering output
+const questions = [
+    {
+        type: "input",
+        name: "letters",
+        message: "Input up to 3 letters."
+    },
+    {
+        type: "input",
+        name: "textColor",
+        message: "Enter a color keyword or hexidecimal."
+
+    },
+    {
+        type: "list",
+        name: "shape",
+        message: "Which shape would you like?",
+        choices: ["circle", "triangle", "square"]
+    },
+    {
+        type: "input",
+        name: "shapeColor",
+        message: "Enter a color keyword or hexidecimal."
+    }
+];
+
+// function that takes data from inquirer and pumps out image. If statements handle the variable choice of shape
+function init() {
+    inquirer.prompt(questions).then(data =>{
+        console.log(data)
+        let logoShape;
+        if(data.shape === "circle"){
+            logoShape = new Circle(data.shapeColor)
+        }
+        else if (data.shape === "triangle"){
+            logoShape = new Triangle(data.shapeColor) //add color
+        } else {
+            logoShape = new Square(data.shapeColor) // ''
+        }
+     
+    //   variable that actually outputs the shape and is taken in by write file function.
+        let svgTag = `
+        <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
+        ${logoShape.render()}
+
+         <text x="150" y="125" font-size="60" text-anchor="middle" fill="${data.textColor}">${data.letters}</text>
+
+        </svg>
+        `
+
+
+        fs.writeFileSync("logo.svg", svgTag);
+
+    })
+    .then((console.log("Generate logo.svg")));
+}
+
+init();
